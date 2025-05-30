76afd4eaad6c04c9bbf825fd4d85ae72bfd14926
ajmnz <arnau@ajb.cat>
2025-05-30 10:36:11 +0200

    fix comments on build

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,6 +1,21 @@
+import type arrType from "./arr";
 import { Bitar } from "./bitar";
+import type dateType from "./date";
+import type numType from "./num";
+import type objType from "./obj";
+import type promType from "./prom";
+import type strType from "./str";
 
-export const { arr, prom, str, date, obj, num, configure } = new Bitar();
+// this is necessary to maintain comments, otherwise ts loses the source
+// comments and strips them out
+const b = new Bitar();
+export const arr: ReturnType<typeof arrType> = b.arr;
+export const prom: ReturnType<typeof promType> = b.prom;
+export const str: ReturnType<typeof strType> = b.str;
+export const date: ReturnType<typeof dateType> = b.date;
+export const obj: ReturnType<typeof objType> = b.obj;
+export const num: ReturnType<typeof numType> = b.num;
+export const configure: Bitar["configure"] = b.configure;
 
 export default function bitar(...args: ConstructorParameters<typeof Bitar>) {
   return new Bitar(...args);
